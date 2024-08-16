59f03ceb719f3600cb18f8d7204c7f148d597b99
Kelvin Musodza <kelvinledoit@gmail.com>
2024-08-16 12:11:10 -0700

    Update ExpandedFixMessageTest.java

--- src/test/java/exchange/core2/core/ExpandedFixMessageTest.java
+++ src/test/java/exchange/core2/core/ExpandedFixMessageTest.java
@@ -1,5 +1,15 @@
 package exchange.core2.core;
-
+import exchange.core2.core.common.*;
+import exchange.core2.core.common.api.ApiAddUser;
+import exchange.core2.core.common.api.ApiAdjustUserBalance;
+import exchange.core2.core.common.api.ApiPlaceOrder;
+import exchange.core2.core.common.api.binary.BatchAddSymbolsCommand;
+import exchange.core2.core.common.api.reports.SingleUserReportQuery;
+import exchange.core2.core.common.api.reports.SingleUserReportResult;
+import exchange.core2.core.common.api.reports.TotalCurrencyBalanceReportQuery;
+import exchange.core2.core.common.api.reports.TotalCurrencyBalanceReportResult;
+import exchange.core2.core.common.cmd.CommandResultCode;
+import exchange.core2.core.common.config.ExchangeConfiguration;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import quickfix.*;
@@ -10,6 +20,7 @@ import quickfix.fix44.OrderCancelRequest;
 import quickfix.fix44.QuoteRequest;
 
 import java.io.InputStream;
+import java.util.concurrent.CompletableFuture;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
@@ -67,7 +78,206 @@ public class ExpandedFixMessageTest {
         });
     }
 
-    private void runFixClientTest(FixMessageSender sender) throws Exception {
+    @Test
+    public void testPostTradeData() throws Exception {
+        // Initialize events processor and exchange core as shown in ITCoreExample
+
+        SimpleEventsProcessor eventsProcessor = new SimpleEventsProcessor(new IEventsHandler() {
+            @Override
+            public void tradeEvent(TradeEvent tradeEvent) {
+                System.out.println("Trade event: " + tradeEvent);
+            }
+
+            @Override
+            public void reduceEvent(ReduceEvent reduceEvent) {
+                System.out.println("Reduce event: " + reduceEvent);
+            }
+
+            @Override
+            public void rejectEvent(RejectEvent rejectEvent) {
+                System.out.println("Reject event: " + rejectEvent);
+            }
+
+            @Override
+            public void commandResult(ApiCommandResult commandResult) {
+                System.out.println("Command result: " + commandResult);
+            }
+
+            @Override
+            public void orderBook(OrderBook orderBook) {
+                System.out.println("OrderBook event: " + orderBook);
+            }
+        });
+
+        ExchangeCore exchangeCore = ExchangeCore.builder()
+                .resultsConsumer(eventsProcessor)
+                .exchangeConfiguration(ExchangeConfiguration.defaultBuilder().build())
+                .build();
+
+        exchangeCore.startup();
+
+        ExchangeApi api = exchangeCore.getApi();
+        int currencyCodeXbt = 11;
+        int currencyCodeLtc = 15;
+        int symbolXbtLtc = 241;
+
+        // Add symbols, users, and balances (similar to ITCoreExample)
+
+        // Place orders and trade
+
+        // Get the post-trade data
+        CompletableFuture<L2MarketData> orderBookFuture = api.requestOrderBookAsync(symbolXbtLtc, 10);
+        L2MarketData orderBookData = orderBookFuture.get();
+        System.out.println("Order book after trades: " + orderBookData);
+
+        CompletableFuture<SingleUserReportResult> report1 = api.processReport(new SingleUserReportQuery(301), 0);
+        System.out.println("User 301 balances: " + report1.get().getAccounts());
+
+        CompletableFuture<SingleUserReportResult> report2 = api.processReport(new SingleUserReportQuery(302), 0);
+        System.out.println("User 302 balances: " + report2.get().getAccounts());
+
+        CompletableFuture<TotalCurrencyBalanceReportResult> totalsReport = api.processReport(new TotalCurrencyBalanceReportQuery(), 0);
+        System.out.println("LTC fees collected: " + totalsReport.get().getFees().get(currencyCodeLtc));
+
+        // Shutdown exchange core after test
+        exchangeCore.shutdown();
+    }
+
+    @Test
+    public void testBondTradeScenario() throws Exception {
+        // Initialize events processor and exchange core
+
+        SimpleEventsProcessor eventsProcessor = new SimpleEventsProcessor(new IEventsHandler() {
+            @Override
+            public void tradeEvent(TradeEvent tradeEvent) {
+                System.out.println("Trade event: " + tradeEvent);
+            }
+
+            @Override
+            public void reduceEvent(ReduceEvent reduceEvent) {
+                System.out.println("Reduce event: " + reduceEvent);
+            }
+
+            @Override
+            public void rejectEvent(RejectEvent rejectEvent) {
+                System.out.println("Reject event: " + rejectEvent);
+            }
+
+            @Override
+            public void commandResult(ApiCommandResult commandResult) {
+                System.out.println("Command result: " + commandResult);
+            }
+
+            @Override
+            public void orderBook(OrderBook orderBook) {
+                System.out.println("OrderBook event: " + orderBook);
+            }
+        });
+
+        ExchangeCore exchangeCore = ExchangeCore.builder()
+                .resultsConsumer(eventsProcessor)
+                .exchangeConfiguration(ExchangeConfiguration.defaultBuilder().build())
+                .build();
+
+        exchangeCore.startup();
+
+        ExchangeApi api = exchangeCore.getApi();
+        int currencyCodeUsd = 840; // USD
+        int currencyCodeBonds = 1001; // Custom bond currency code
+        int symbolBondTrade = 5001; // Symbol for bond trades
+
+        // Add a bond symbol
+        CoreSymbolSpecification bondSymbolSpec = CoreSymbolSpecification.builder()
+                .symbolId(symbolBondTrade)
+                .type(SymbolType.BOND)
+                .baseCurrency(currencyCodeBonds)
+                .quoteCurrency(currencyCodeUsd)
+                .baseScaleK(1000L)
+                .quoteScaleK(100L)
+                .takerFee(500L)
+                .makerFee(200L)
+                .build();
+
+        CompletableFuture<CommandResultCode> future = api.submitBinaryDataAsync(new BatchAddSymbolsCommand(bondSymbolSpec));
+        System.out.println("BatchAddSymbolsCommand result: " + future.get());
+
+        // Create users
+        future = api.submitCommandAsync(ApiAddUser.builder().uid(1001L).build());
+        System.out.println("ApiAddUser 1 result: " + future.get());
+
+        future = api.submitCommandAsync(ApiAddUser.builder().uid(1002L).build());
+        System.out.println("ApiAddUser 2 result: " + future.get());
+
+        // User 1001 deposits USD
+        future = api.submitCommandAsync(ApiAdjustUserBalance.builder()
+                .uid(1001L)
+                .currency(currencyCodeUsd)
+                .amount(200_000_000L) // $1,000,000
+                .transactionId(1L)
+                .build());
+
+        System.out.println("ApiAdjustUserBalance 1 result: " + future.get());
+
+        // User 1002 deposits bonds
+        future = api.submitCommandAsync(ApiAdjustUserBalance.builder()
+                .uid(1002L)
+                .currency(currencyCodeBonds)
+                .amount(500_000L) // 500 bonds
+                .transactionId(2L)
+                .build());
+
+        System.out.println("ApiAdjustUserBalance 2 result: " + future.get());
+
+        // User 1001 places a Good-till-Cancel Bid order to buy bonds
+        future = api.submitCommandAsync(ApiPlaceOrder.builder()
+                .uid(1001L)
+                .orderId(3001L)
+                .price(102L) // Price of $102 per bond
+                .size(200L) // 200 bonds
+                .action(OrderAction.BID)
+                .orderType(OrderType.GTC)
+                .symbol(symbolBondTrade)
+                .build());
+
+        System.out.println("ApiPlaceOrder 1 result: " + future.get());
+
+        // User 1002 places an Immediate-or-Cancel Ask order to sell bonds
+        future = api.submitCommandAsync(ApiPlaceOrder.builder()
+                .uid(1002L)
+                .orderId(3002L)
+                .price(101L) // Price of $101 per bond
+                .size(150L) // 150 bonds
+                .action(OrderAction.ASK)
+                .orderType(OrderType.IOC)
+                .symbol(symbolBondTrade)
+                .build());
+
+        System.out.println("ApiPlaceOrder 2 result: " + future.get());
+
+        // Request order book
+        CompletableFuture<L2MarketData> orderBookFuture = api.requestOrderBookAsync(symbolBondTrade, 10);
+        L2MarketData orderBookData = orderBookFuture.get();
+        System.out.println("Order book after trades: " + orderBookData);
+
+        // Get user balance reports
+        CompletableFuture<SingleUserReportResult> report1 = api.processReport(new SingleUserReportQuery(1001), 0);
+        System.out.println("User 1001 balances: " + report1.get().getAccounts());
+
+        CompletableFuture<SingleUserReportResult> report2 = api.processReport(new SingleUserReportQuery(1002), 0);
+        System.out.println("User 1002 balances: " + report2.get().getAccounts());
+
+        // Check fees collected
+        CompletableFuture<TotalCurrencyBalanceReportResult> totalsReport = api.processReport(new TotalCurrencyBalanceReportQuery(), 0);
+        System.out.println("USD fees collected: " + totalsReport.get().getFees().get(currencyCodeUsd));
+
+        // Shutdown exchange core after test
+        exchangeCore.shutdown();
+    }
+
+
+
+
+private void runFixClientTest(FixMessageSender sender) throws Exception {
         // Load FIX session settings
         InputStream inputStream = ExpandedFixMessageTest.class.getResourceAsStream("/fixclient.cfg");
 
